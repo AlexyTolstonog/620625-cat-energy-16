f1bd480eae405c226894df085930136645b0f154
AlexyLexto92 <lexto92@gmail.com>
2019-05-11 23:06:33 +0300

    8задание

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task("html", function () {
     server.init({
     server: "build/"
     });
-
+    
     gulp.watch("source/less/**/*.less", gulp.series("css"));
     gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
     gulp.watch("source/*.html", gulp.series("html", "refresh"));
